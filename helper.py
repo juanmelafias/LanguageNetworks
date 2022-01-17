05089a7ca917641f265ecce3df7a45b89cd20935
brigan <brigan@gmail.com>
2022-01-18 00:14:14 +0100

    Added closenessCentrality and harmonicCentrality. Attempted to add katzCentrality but it doesn't converge. Started to automate average measurements over neighbors. averageNeighborDegree is left out at the moment because of this.

--- helper.py
+++ helper.py
@@ -34,93 +34,155 @@ from copy import copy;
 from scipy.stats import entropy; 
 
 
-def computeNodesStatistics(net, fVerbose=False): 
-	"""	computeNodesStatistics function: 
+def computeNodesProperties(net, fVerbose=False): 
+	"""	computeNodesProperties function: 
 
-			This function computes statistics for all the nodes of the network provided. 
+			This function computes a series of properties of all the nodes of the network provided. Some of these
+			properties might be problematic (e.g. because they are the same for all nodes, or they are ill-defined
+			in the network). This function flags problematic properties so that they are not included in the analysis. 
 
 			ACHTUNG!! 
-				The measured statistics and nodes must be in the same order to be able to plot things properly. 
+				The measured properties and nodes must be in the same order to be able to plot things properly. 
 
 			Inputs: 
 				>> net: Upon whose nodes we wish to measure stuff. 
 				>> fVerbose=False: To indicate whether we want to read what is being computed at each moment. 
+					> Not used at the moment. Decided to skip verbosity altogether. 
 
 			Returns: 
-				<< nodesStatistics: stuff measured on each and every node. 
+				<< nodeList: List of nodes as listed when called from the Graph() object. 
+				<< nodesProperties: Dictionary containing all the measurements performed on the nodes. 
+				<< includedProperties: List of non-problematic measurements. 
+				<< excludedProperties: List of problematic measurements. 
+					- includedProperties + excludedProperties = nodesProperties.keys(). 
 
 	"""
 
+	## Pre-processing the network: 
+
+	# Extracting a list of nodes as they appear when called from the net object. 
 	nodeList = net.nodes(); 
 	nNodes = len(nodeList); 
-	allStatistics = ["averageNeighborDegree", "degreeCentrality", "eigenvectorCentrality", "betweennessCentrality"]; 
-	allStatistics += ["clustering", "componentSize", "coreNumber", "pagerank", "degree", "normalizedDegree"]; 
-	## ACH! The next line is the one currently used for syntax network. This should be reviewed. 
-	## 	The problem comes with "componentSize", which is the same for all if all are connected. 
-	# allStatistics += ["componentSize", "coreNumber", "pagerank", "degree", "normalizedDegree"]; 
-	## Component size is substituted by "clustering", which is troublesome for some sintex networks. 
-	## Other kinds of networks have issues with coreNumber, because all nodes belong to the same largest k-core. 
-	## We have to solve the invariant dimension issue. 
-	# allStatistics += ["clustering", "coreNumber", "pagerank", "degree", "normalizedDegree"]; 
-	# allStatistics += ["clustering", "pagerank", "degree", "normalizedDegree"]; 
-	nodesStatistics = {}; 
-	for statistic in allStatistics: 
-		nodesStatistics[statistic] = np.zeros([nNodes, 1]).squeeze(); 
 
-	# Network without self loops: 
+	# ACHTUNG!! 
+	# 	Extracting the largest connected component. 
+	# 	In the future, this should be avoided. Problematic properties (e.g. average path length to each node) should
+	# 	be automatically flagged and sorted out by this function. 
+	thisGCC = max(nx.connected_components(net), key=len); 
+
+	# ACHTUNG!! 
+	# 	Some of the properties measured demand that networks have no self loops. 
+	# 	At the moment this is left to keep the function working, but I should revise it at some point. 
+	# 	Ideally, if a measure is problematic because of self-loops, this should be flagged and reported in excludedProperties. 
 	netWOSL = copy(net); 
 	netWOSL.remove_edges_from(nx.selfloop_edges(netWOSL)); 
 
-	# print("Average neighbor degree"); 
-	thisAND = nx.average_neighbor_degree(net); 
-	# print("Degree centrality"); 
-	thisDC = nx.degree_centrality(net); 
-	# print("Eigenvector centrality"); 
+
+	## Initializing properties measured upon nodes: 
+
+	# A list of primary properties: 
+	primaryProperties = ["degreeCentrality", "eigenvectorCentrality", "betweennessCentrality"]; 
+	primaryProperties += ["closenessCentrality", "harmonicCentrality"]; 
+	primaryProperties += ["clustering", "componentSize", "coreNumber", "pagerank", "degree", "normalizedDegree"]; 
+	measuredProperties = copy(primaryProperties); 
+
+	# # These are complemented by average properties of the neighbors to measure if similar nodes connect (as in
+	# # assortativity) and standard deviation of properties of neighbors to measure whether connection is specific or
+	# # heterogeneous. 
+	# for thisProperty in primaryProperties: 
+	# 	measuredProperties += [thisProperty+"_neighborMean", thisProperty+"_neighborStd"]; 
+
+
+	## Initializing the dictionary that will contain all properties: 
+	nodesProperties = {}; 
+	for thisProperty in measuredProperties: 
+		nodesProperties[thisProperty] = np.zeros([nNodes, 1]).squeeze(); 
+
+
+	## Performing the actual computations: 
+
+	# ACHTUNG!! 
+	# 	Degree centrality: 
+	# 	This is redundant as it is just the node degree normalized by the number of nodes in the network. 
+	# 	Should be removed. Kept for the moment. 
+	thisDegreeCentrality = nx.degree_centrality(net); 
+
+	# Eigenvector centrality: 
+	# 	ACH! Remind why this exception! 
 	try: 
-		thisEC = nx.eigenvector_centrality(net); 
+		thisEigenvectorCentrality = nx.eigenvector_centrality(net); 
 	except: 
-		thisEC = nx.eigenvector_centrality(net, max_iter=10000); 
-	# print("Betweenness centrality"); 
-	thisBC = nx.betweenness_centrality(net); 
-	# print("Clustering"); 
+		thisEigenvectorCentrality = nx.eigenvector_centrality(net, max_iter=10000); 
+
+	# Betweenness centrality: 
+	thisBetweennessCentrality = nx.betweenness_centrality(net); 
+
+	# Closeness centrality: 
+	thisClosenessCentrality = nx.closeness_centrality(net); 
+
+	# Harmonic centrality: 
+	thisHarmonicCentrality = nx.harmonic_centrality(net); 
+
+	# # ACHTUNG!! Excluded. It does not converge! 
+	# # Katz centrality: 
+	# try: 
+	# 	thisKatzCentrality = nx.katz_centrality(net); 
+	# except: 
+	# 	thisKatzCentrality = nx.katz_centrality(net, max_iter=10000); 
+
+	# Clustering coefficient: 
 	thisClustering = nx.clustering(net); 
-	# print("Largest connected component"); 
-	thisGCC = max(nx.connected_components(net), key=len); 
-	# print("Core number"); 
-	thisKN = nx.core_number(netWOSL); 
+
+	# Size of largest k-core to which each node belongs: 
+	thisLargestKCore = nx.core_number(netWOSL); 
+
 	# thisEccentricity = nx.eccentricity(net); 
-	# print("Page rank"); 
-	thisPR = nx.pagerank(net); 
-	# print("Degree"); 
+	
+	# Page rank: 
+	thisPagerank = nx.pagerank(net); 
+
+	# Node degree: 
 	thisDegree = net.degree(); 
 
-	for (iNode, node) in enumerate(nodeList): 
-		nodesStatistics["averageNeighborDegree"][iNode] = thisAND[node]; 
-		nodesStatistics["degreeCentrality"][iNode] = thisDC[node]; 
-		nodesStatistics["eigenvectorCentrality"][iNode] = thisEC[node]; 
-		nodesStatistics["betweennessCentrality"][iNode] = thisBC[node]; 
-		nodesStatistics["clustering"][iNode] = thisClustering[node]; 
-		nodesStatistics["componentSize"][iNode] = float(len(nx.node_connected_component(net, node)))/len(thisGCC); 
-		nodesStatistics["coreNumber"][iNode] = thisKN[node]; 
-		nodesStatistics["pagerank"][iNode] = thisPR[node]; 
-		nodesStatistics["degree"][iNode] = thisDegree[node]; 
 
-	nodesStatistics["normalizedDegree"] = nodesStatistics["degree"]*2/sum(nodesStatistics["degree"]); 
+	# print("Average neighbor degree"); 
+	thisAND = nx.average_neighbor_degree(net); 
+
+	for (iNode, node) in enumerate(nodeList): 
+		nodesProperties["degreeCentrality"][iNode] = thisDegreeCentrality[node]; 
+		nodesProperties["eigenvectorCentrality"][iNode] = thisEigenvectorCentrality[node]; 
+		nodesProperties["betweennessCentrality"][iNode] = thisBetweennessCentrality[node]; 
+		nodesProperties["closenessCentrality"][iNode] = thisClosenessCentrality[node]; 
+		nodesProperties["harmonicCentrality"][iNode] = thisHarmonicCentrality[node]; 
+		nodesProperties["clustering"][iNode] = thisClustering[node]; 
+		nodesProperties["componentSize"][iNode] = float(len(nx.node_connected_component(net, node)))/len(thisGCC); 
+		nodesProperties["coreNumber"][iNode] = thisLargestKCore[node]; 
+		nodesProperties["pagerank"][iNode] = thisPagerank[node]; 
+		nodesProperties["degree"][iNode] = thisDegree[node]; 
+		# nodesProperties["averageNeighborDegree"][iNode] = thisAND[node]; 
+
+	nodesProperties["normalizedDegree"] = nodesProperties["degree"]*2/sum(nodesProperties["degree"]); 
+
+	# for (iNode, node) in enumerate(nodeList): 
+	# 	nodeNeighbors = net.neighbors(node); 
+	# 	for thisProperty in primaryProperties: 
+	# 		neighborProperty = nodesProperties[]
+
 
 	# Reporting which statistics are problematic. 
 	# These can be problematic, e.g., because there is no variation. Then, they do not contribute to any PC. 
 	# These would just have a zero on the eigenvectors, but algebra cannot handle these cases properly. 
-	includedStatistics = []; 
-	excludedStatistics = []; 
-	for statistic in nodesStatistics.keys(): 
-		thisMean = np.mean(nodesStatistics[statistic]); 
-		thisStd = np.std(nodesStatistics[statistic]); 
+	includedProperties = []; 
+	excludedProperties = []; 
+	for statistic in nodesProperties.keys(): 
+		thisMean = np.mean(nodesProperties[statistic]); 
+		thisStd = np.std(nodesProperties[statistic]); 
 		if ((np.isnan(thisMean)) or (thisStd == 0.) or (thisStd/thisMean < 10e-11)): 
-			excludedStatistics += [statistic]; 
+			excludedProperties += [statistic]; 
 		else: 
-			includedStatistics += [statistic]; 
+			includedProperties += [statistic]; 
 
-	return (nodeList, nodesStatistics, includedStatistics, excludedStatistics); 
+	return (nodeList, nodesProperties, includedProperties, excludedProperties); 
 
 
 def convertPC2RGB(thisArray): 
--- script_testNDM.py
+++ script_testNDM.py
@@ -89,43 +89,40 @@ location = "home";
 # thisNetwork = allNetworksDict[thisKey][allNetworksNamesDict[thisKey][iNetwork]]; 
 
 
-#######################################################################################################################
-# Uncomment for randomly generated networks: 
-
-thisKey = "None"; 
-# thisNetwork = nx.erdos_renyi_graph(200, 0.1); 
-# thisNetwork = nx.watts_strogatz_graph(100, 4, 0.05); 
-# thisNetwork = nx.barabasi_albert_graph(200, 2); 
-thisNetwork = nx.bipartite.gnmk_random_graph(50,50,200); 
-
-
 # #######################################################################################################################
-# # Uncomment for CNB network: 
+# # Uncomment for randomly generated networks: 
 
-# thisKey = "None"; 
-# dataPath = "/home/brigan/Desktop/Research_CNB/Misc/CNB_net/Code/Output/"; 
+# # thisNetwork = nx.erdos_renyi_graph(200, 0.1); 
+# # thisNetwork = nx.watts_strogatz_graph(100, 4, 0.05); 
+# # thisNetwork = nx.barabasi_albert_graph(200, 2); 
+# thisNetwork = nx.bipartite.gnmk_random_graph(50,50,200); 
 
-# # Reading edges: 
-# fIn	= open(dataPath + "edges.csv", 'r'); 
-# edges = []; 
-# nodes = []; 
-# allLines = fIn.read().splitlines(); 
-# nPapers = {}; 
-# nCollaborations = {}; 
-# for line in allLines: 
-# 	thisEdge = line.split(', '); 
-# 	edges += [(thisEdge[0], thisEdge[1])]; 
 
-# # Building network from edges: 
-# thisNetwork = nx.Graph(); 
-# thisNetwork.add_edges_from(edges); 
+#######################################################################################################################
+# Uncomment for CNB network: 
+
+dataPath = "/home/brigan/Desktop/Research_CNB/Misc/CNB_net/Code/Output/"; 
+
+# Reading edges: 
+fIn	= open(dataPath + "edges.csv", 'r'); 
+edges = []; 
+nodes = []; 
+allLines = fIn.read().splitlines(); 
+nPapers = {}; 
+nCollaborations = {}; 
+for line in allLines: 
+	thisEdge = line.split(', '); 
+	edges += [(thisEdge[0], thisEdge[1])]; 
+
+# Building network from edges: 
+thisNetwork = nx.Graph(); 
+thisNetwork.add_edges_from(edges); 
 
 
 
 # ########################################################################################################################
 # ## Uncomment for connectome network: 
 
-# thisKey = "None"; 
 # connectomeDataPath = "/home/brigan/Desktop/Research_CNB/Networks/Networks/Connectome/"; 
 # # thisNetwork = nx.read_graphml(connectomeDataPath + "993675_repeated10_scale250.graphml"); 
 # # thisNetwork = nx.read_graphml(connectomeDataPath + "958976_repeated10_scale250.graphml"); # Check out this network!! Compare to others! 
@@ -149,13 +146,14 @@ nNodes = len(thisNetwork.nodes());
 
 
 # Measure stuff from nodes: 
-(nodeList, nodesStatistics, includedStatistics, excludedStatistics) = h.computeNodesStatistics(thisNetwork); 
+(nodeList, nodesProperties, includedProperties, excludedProperties) = h.computeNodesProperties(thisNetwork); 
 
-nStatistics = len(includedStatistics); 
-allStatisticsArray = np.zeros([nStatistics, nNodes]); 
+
+nProperties = len(includedProperties); 
+allPropertiesArray = np.zeros([nProperties, nNodes]); 
 dictIStat = {}; 
-for (iStat, statistic) in enumerate(includedStatistics): 
-	allStatisticsArray[iStat,:] = nodesStatistics[statistic]; 
+for (iStat, statistic) in enumerate(includedProperties): 
+	allPropertiesArray[iStat,:] = nodesProperties[statistic]; 
 	dictIStat[statistic] = iStat; 
 
 
@@ -163,13 +161,13 @@ for (iStat, statistic) in enumerate(includedStatistics):
 
 
 # # Standardizing distro: 
-allStatisticsMean = np.mean(allStatisticsArray, 1); 
-allStatisticsStd = np.std(allStatisticsArray, 1); 
-allStatisticsArray = allStatisticsArray - np.transpose(np.repeat(np.array([allStatisticsMean]), nNodes, 0)); 
-allStatisticsArray = np.divide(allStatisticsArray, np.transpose(np.repeat(np.array([allStatisticsStd]), nNodes, 0))); 
+allStatisticsMean = np.mean(allPropertiesArray, 1); 
+allStatisticsStd = np.std(allPropertiesArray, 1); 
+allPropertiesArray = allPropertiesArray - np.transpose(np.repeat(np.array([allStatisticsMean]), nNodes, 0)); 
+allPropertiesArray = np.divide(allPropertiesArray, np.transpose(np.repeat(np.array([allStatisticsStd]), nNodes, 0))); 
 
 # Computing correlation matrix and diagonalizing: 
-allStatisticsCov = np.cov(allStatisticsArray); 
+allStatisticsCov = np.cov(allPropertiesArray); 
 # (eigVals, eigVects) = la.eig(allStatisticsCov); 	# ACHTUNG!! This seems to produce more imaginary component when it shouldn't... 
 (eigVals, eigVects) = np.linalg.eig(allStatisticsCov); 
 # eigVals = np.real(eigVals); 
@@ -200,14 +198,14 @@ plt.colorbar();
 
 
 # Projecting data into eigenspace: 
-allStatisticsArray_ = np.dot(np.transpose(eigVects), allStatisticsArray); 
+allPropertiesArray_ = np.dot(np.transpose(eigVects), allPropertiesArray); 
 
 
 ## Using first three PCs as color coding: 
 # Normalize components to [0,1]; 
-valuesRGB0 = h.convertPC2RGB(allStatisticsArray_[0,:]); 
-valuesRGB1 = h.convertPC2RGB(allStatisticsArray_[1,:]); 
-valuesRGB2 = h.convertPC2RGB(allStatisticsArray_[2,:]); 
+valuesRGB0 = h.convertPC2RGB(allPropertiesArray_[0,:]); 
+valuesRGB1 = h.convertPC2RGB(allPropertiesArray_[1,:]); 
+valuesRGB2 = h.convertPC2RGB(allPropertiesArray_[2,:]); 
 # Save hex color values to a list: 
 nodeColor = []; 
 for (iNode, node) in enumerate(nodeList): 
@@ -218,24 +216,24 @@ for (iNode, node) in enumerate(nodeList):
 # PC1-PC2: 
 fig = plt.figure(); 
 ax = fig.add_subplot(111); 
-plt.scatter(allStatisticsArray_[0,:], allStatisticsArray_[1,:], c=nodeColor); 
+plt.scatter(allPropertiesArray_[0,:], allPropertiesArray_[1,:], c=nodeColor); 
 # for (iWord, word) in enumerate(nodeList): 
-# 	ax.annotate(word, (allStatisticsArray_[0,iWord], allStatisticsArray_[1,iWord])); 
-	# plt.text(allStatisticsArray_[0,iWord], allStatisticsArray_[1,iWord], word); 
+# 	ax.annotate(word, (allPropertiesArray_[0,iWord], allPropertiesArray_[1,iWord])); 
+	# plt.text(allPropertiesArray_[0,iWord], allPropertiesArray_[1,iWord], word); 
 plt.xlabel("PC1"); 
 plt.ylabel("PC2"); 
 
 
 # PC1-PC3: 
 fig = plt.figure(); 
-plt.scatter(allStatisticsArray_[0,:], allStatisticsArray_[2,:], c=nodeColor); 
+plt.scatter(allPropertiesArray_[0,:], allPropertiesArray_[2,:], c=nodeColor); 
 plt.xlabel("PC1"); 
 plt.ylabel("PC3"); 
 
 # PC1-PC2-PC3: 
 fig = plt.figure(); 
 ax = fig.add_subplot(111, projection='3d'); 
-ax.scatter(allStatisticsArray_[0,:], allStatisticsArray_[1,:], allStatisticsArray_[2,:], c=nodeColor); 
+ax.scatter(allPropertiesArray_[0,:], allPropertiesArray_[1,:], allPropertiesArray_[2,:], c=nodeColor); 
 ax.set_xlabel("PC1"); 
 ax.set_ylabel("PC2"); 
 ax.set_zlabel("PC3"); 
@@ -262,13 +260,13 @@ plt.show();
 # sys.exit(0); 
 
 # # Saving data in eigenspace: 
-# np.savetxt(os.path.join(dataPathMaster, "Results/DataForPCA/dataEigenSpace.csv"), allStatisticsArray_, delimiter=", "); 
+# np.savetxt(os.path.join(dataPathMaster, "Results/DataForPCA/dataEigenSpace.csv"), allPropertiesArray_, delimiter=", "); 
 # np.savetxt(os.path.join(dataPathMaster, "Results/DataForPCA/eigenValues.csv"), eigVals, delimiter=", "); 
 # np.savetxt(os.path.join(dataPathMaster, "Results/DataForPCA/eigenVectors.csv"), eigVects, delimiter=", "); 
 # np.savetxt(os.path.join(dataPathMaster, "Results/DataForPCA/covariance.csv"), allStatisticsCov, delimiter=", "); 
 
 # ## Saving data to files: 
-# np.savetxt(os.path.join(dataPathMaster, "Results/DataForPCA/data.csv"), allStatisticsArray, delimiter=", "); 
+# np.savetxt(os.path.join(dataPathMaster, "Results/DataForPCA/data.csv"), allPropertiesArray, delimiter=", "); 
 # for key in netIndexes.keys(): 
 # 	fOut = open(os.path.join(dataPathMaster, "Results/DataForPCA/labels"+key+".csv"), 'w'); 
 # 	for (iIndex, index) in enumerate(netIndexes[key]): 
