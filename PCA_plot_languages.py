10e6652331eeb9b4c820b25650584cd6a4ae2cc0
JuanLangoo <juan.soria@langoo.io>
2022-06-20 01:53:04 +0200

    more changes

---
+++ PCA_plot_languages.py
@@ -0,0 +1,216 @@
+# Importing relevant libraries:
+# import pandas as pd
+# Importing libraries for I/O and system communication:
+import os
+
+import matplotlib as mplt
+import matplotlib.pyplot as plt
+import numpy as np
+import scipy.cluster.hierarchy as spc
+
+# Importing homebrew libraries:
+import helper as h
+from utils import build_properties_array_languages
+
+# from copy import copy;
+# For 3D scatter:
+# from mpl_toolkits.mplot3d import Axes3D;
+
+
+# import pickle as pkl;
+# import scipy.io as sio; # To read .mat files! and .mnx files!
+
+# Importing functions for clustering:
+# from sklearn.cluster import KMeans;
+# import scipy.cluster.hierarchy as spc;
+
+
+# import loadHelper as lh;
+
+
+clusterStyles = {}
+clusterStyles[0] = "k"
+clusterStyles[1] = "r"
+clusterStyles[2] = "g"
+clusterStyles[3] = "b"
+clusterStyles[4] = "y"
+clusterStyles[5] = "m"
+clusterStyles[6] = "c"
+clusterStyles[7] = "tab:gray"
+
+primaries = True
+
+(valid1, arraymeanproperties, dict_pathologies) = build_properties_array_languages(
+    "files/inflected/networks/", primaries
+)
+(
+    valid2,
+    arraymeanpropertieslemma,
+    dict_pathologies_lemma,
+) = build_properties_array_languages("files/lemmatized/networks/", primaries)
+arraymeanproperties = h.normalizeProperties(arraymeanproperties)
+arraymeanpropertieslemma = h.normalizeProperties(arraymeanpropertieslemma)
+if primaries:
+    folderpics = "languagesmeanprimaries"
+else:
+    folderpics = "languagesmeanneighbours"
+paths = [f"files/inflected/{folderpics}/", f"files/lemmatized/{folderpics}/"]
+filelist = os.listdir("./files/inflected/dictionaries/")
+nodeList = [
+    file.split(".")[0]
+    for file in filelist
+    if file not in ["Japanese.json", "Arabic.json", "French.json"]
+]
+valid_keys = [valid1, valid2]
+
+for indexlang, includedPropertiesArray in enumerate(
+    [arraymeanproperties, arraymeanpropertieslemma]
+):
+    picsPath = paths[indexlang]
+    includedProperties = valid_keys[indexlang]
+    allStatisticsCov = np.cov(includedPropertiesArray)
+    (eigVals, eigVects) = np.linalg.eig(allStatisticsCov)
+    eigVals = np.real(eigVals)
+    eigVects = np.real(eigVects)
+
+    # Computing PCs with information above noise level according to ref:
+    # 	Donoho DL, Gavish M.
+    # 	The optimal hard threshold for singular values is 4/√3.
+    # 	arXiv preprint arXiv:1305.5870, (2013).
+    (noiseThreshold, nKeep) = h.computeComponentsAboveNoise(eigVals)
+    print("Noise-trucating PC value is: " + str(noiseThreshold))
+    print("According to this, optimal number of PCs kept is: " + str(nKeep))
+    print(
+        "This is a fraction " + str(float(nKeep) / len(eigVals)) + " of eigenvalues. "
+    )
+
+    # Plotting covariance matrix:
+    plt.figure()
+    plt.imshow(allStatisticsCov, interpolation="none")
+    plt.colorbar()
+
+    # Plotting eigenvectors:
+    plt.figure()
+    plt.imshow(eigVects, interpolation="none", cmap="coolwarm")
+    plt.colorbar()
+
+    # Computing and plotting variance explained:
+    (varianceExplained, varianceExplained_cumul) = h.varianceExplained(eigVals)
+
+    plt.figure()
+    plt.plot(varianceExplained)
+
+    plt.figure()
+    plt.plot(varianceExplained_cumul)
+
+    ## Projecting data into eigenspace:
+    includedPropertiesArray_ = np.dot(np.transpose(eigVects), includedPropertiesArray)
+
+    # Using first three PCs as color coding:
+    # 	Normalize components to [0,1];
+    valuesRGB0 = h.convertPC2RGB(includedPropertiesArray_[0, :])
+    valuesRGB1 = h.convertPC2RGB(includedPropertiesArray_[1, :])
+    valuesRGB2 = h.convertPC2RGB(includedPropertiesArray_[2, :])
+    # Save hex color values to a list:
+    nodeColor = []
+    for (iNode, node) in enumerate(nodeList):
+        nodeColor += [
+            mplt.colors.to_hex(
+                [valuesRGB0[iNode], valuesRGB1[iNode], valuesRGB2[iNode]]
+            )
+        ]
+
+    # PC1-PC2:
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    plt.scatter(
+        includedPropertiesArray_[0, :], includedPropertiesArray_[1, :], c=nodeColor
+    )
+    plt.xlabel("PC1")
+    plt.ylabel("PC2")
+    plt.title("Nodes projected in PCs")
+    fig.savefig(picsPath + "projection_PCs1-2.pdf")
+
+    # PC1-PC3:
+    fig = plt.figure()
+    plt.scatter(
+        includedPropertiesArray_[0, :], includedPropertiesArray_[2, :], c=nodeColor
+    )
+    plt.xlabel("PC1")
+    plt.ylabel("PC3")
+    plt.title("Nodes projected in PCs")
+    fig.savefig(picsPath + "projection_PCs1-3.pdf")
+
+    # PC1-PC2-PC3:
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection="3d")
+    ax.scatter(
+        includedPropertiesArray_[0, :],
+        includedPropertiesArray_[1, :],
+        includedPropertiesArray_[2, :],
+        c=nodeColor,
+    )
+    ax.set_xlabel("PC1")
+    ax.set_ylabel("PC2")
+    ax.set_zlabel("PC3")
+    plt.title("Nodes projected in PCs")
+    fig.savefig(picsPath + "projection_PCs1-2-3.pdf")
+
+    # Plotting in network space:
+    """"
+    fig = plt.figure(); 
+    ax = fig.add_subplot(111); 
+    nx.draw(thisNetwork, with_labels=False, pos=nx.kamada_kawai_layout(thisNetwork), node_color=nodeColor, edge_color="tab:gray"); 
+    ax.set_aspect("equal"); 
+    plt.title("PC colors projected in network layout"); 
+    fig.savefig(picsPath + "networkColoredWithPCs_netLayout.pdf");
+    """
+
+    pdist = spc.distance.pdist(allStatisticsCov)
+    propertiesLinkage = spc.linkage(pdist, method="complete")
+
+    fig = plt.figure()
+    spc.dendrogram(propertiesLinkage, orientation="right", labels=includedProperties)
+    plt.xlabel("Distance")
+    plt.ylabel("Node properties")
+    plt.title("Properties dendrogram")
+    fig.savefig(picsPath + "propertiesDendogram.pdf")
+
+    ## Dendograms for data:
+    nodesLinkage = spc.linkage(includedPropertiesArray_.T, "ward")
+
+    distanceThreshold = 45
+    fig = plt.figure()
+    spc.dendrogram(
+        nodesLinkage,
+        orientation="right",
+        color_threshold=distanceThreshold,
+        labels=nodeList,
+    )
+    plt.xlabel("Distance")
+    plt.ylabel("Nodes")
+    plt.title("Nodes dendrogram")
+    fig.savefig(picsPath + "nodesDendogram.pdf")
+
+    # Coloring according to clusters:
+    # nodeClusters = spc.fcluster(nodesLinkage, distanceThreshold, criterion='distance');
+    nClusters = 5
+    nodeClusters = spc.fcluster(nodesLinkage, nClusters, criterion="maxclust")
+    nodeClusterColor = []
+    for (iNode, node) in enumerate(nodeList):
+        nodeClusterColor += [clusterStyles[nodeClusters[iNode] - 1]]
+
+    # Plotting in eigenspace:
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection="3d")
+    ax.scatter(
+        includedPropertiesArray_[0, :],
+        includedPropertiesArray_[1, :],
+        includedPropertiesArray_[2, :],
+        c=nodeClusterColor,
+    )
+    ax.set_xlabel("PC1")
+    ax.set_ylabel("PC2")
+    ax.set_zlabel("PC3")
+    plt.title("Clusters (dendogram) in eigenspace")
+    fig.savefig(picsPath + "dendogramClusters_eigenspace.pdf")
--- README.md
+++ README.md
@@ -1,3 +1,46 @@
 # Language Networks
 
-This repository is intented for the creation and Analysis of syntactical Language Networks. Syntactical networks are built using syntactic relationships between words, differently from adajacency networks
+This repository is intented for the creation and Analysis of syntactical Language Networks. Syntactical networks are built using syntactic relationships between words, differently from adjacency networks, which are built from words which are adjacent in a sentence. 
+
+The repo stores lots of code and files, most of them might be useful or not
+
+To get all set up to run the scripts run ./setup.sh on your command line
+
+## Files
+Here are stored files derived from the Language Annotated Corpora. If such files are to be generated again from scratch, it will be needed the UsefulData folder, located in the repo root directory. This can be obtained by getting in touch with me.
+
+The Files folder contains to folders itself: inflected and lemmatized. Each contains the same kind of data but for networks constructed from inflected and lemmatized forms respectively. 
+
+### dictionaries and dataframes
+
+So, starting from the .conllu files with the syntacticlly annotated data stored in UsefulData, we can generate the first data about our network. This can be done by running **NetworkGenerator.py**. This calls the file_generator() function stored in **utils.py**. This function takes three parameters: *routes_file*, which is a string with the location of a csv file wich contains the location and basic info of the most important corpus of each language in the UsefulData folder; *num_lines*, which sets the threshold of minimum number of lines for a corpus to be read, and generated files from; and *lemmatized*, which if sets to True creates networks based on lemmatized forms intead of inflected.
+
+This script will create two kind of files: "{name_of_language}.json" and "{name_of_language}.csv" files. The former will be stored in the ***dictionaries/*** folder, since when read back it is expected to be a dictionary where the keys are the nodes of the network, and its values are the other each node is connected to. The latter will be stored in the ***dataframes/***, since whenever is read back is intended to be a Pandas DataFrame with four columns: *word_id*, which gives the node in the network; *word*, which maps the id to the word; *POS*, which gives the part of speech tag of the word, helping thus differencing homographs; and *count*, which provides the number of times each word appears in the corpus in the first *num_lines* lines.
+
+### networks and avgproperties 
+
+Once the files in ***dictionaries/*** and ***dataframes/*** are generated, we can start calculating the properties of the network. This is done massively for all languages stored in ***dictionaries/*** with the aid of **meanproperties.py**. This script allows to compute the properties for each node of the network and the average properties of the node. If your properties are already calculated and you only need to compute the average by reading them first, set CreateProperties to False. Before going any further it is important to state that data is extracted from the files in ***dictionaries/*** and ***dataframes/*** with the functions *json2dict* and *csv2df* in **utils.py**. *json2dict* is pretty standard, simply recuperates the exactly same dictionary that was generated with the connections in the network, but *csv2df* does not retrieve the exact same df it came from. It eliminates the interjections, symbols, punctuation marks and vocatives from the dataframe first, and then sorts the words by its count number, only to finally retrieve the most repeated *wordsnumber* words, which is set to 500 by default. With the function *load_networks* from **utils.py** we create a networkx network, from which we create a subgraph with only the words in the retrieved dataframe in *csv2df*. After that, the greatest connected component of the network is created, and at that point the properties are calculated and then saved using the functions from **helper.py** *computeNodesProperties* and *writeNetworkProperties*. The results are stored in ***networks/***. After that the properties of each network are read from those files, and its node avegrage values are computed and stored in ***avgproperties/***
+
+### languagesmean
+
+In this folder are stored the results from running **PCA_plot_languages.py**. This script is also responsible for computing the numpy array that stores the data with the average properties for all Languages. By tuning the Boolean *primaries* we store the results for the primary properties plus the mean degree of the neighbours in the directory ***languagesmeanprimaries/***. On the contrary, if we wish to compute results based on all 48 properties, we shall set *primaries* to false and results will be stored in ***languagesmeanneighbours/
+***
+
+## Notebooks
+
+They are ther primary for experimentation. Ignore them
+
+## App
+
+Folder for future visualization app with streamlit
+
+## csvroutes
+
+csv useful for certain parts of the code
+
+## reqs
+
+Requirements of python packages necessary to run the app
+## Scripts
+This folder stores a series of python scripts useful for Network Analysys taken from brigan/NodeMorphospaces Github repository
+
