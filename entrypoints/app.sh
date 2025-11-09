6def8ec01e273571317fd0d738d734c02f5c72cf
juanmelafias <juanmelafias@gmail.com>
2025-11-09 13:56:52 +0100

    set port to 8501

--- entrypoints/app.sh
+++ entrypoints/app.sh
@@ -4,4 +4,5 @@ cd /src || exit
 
 echo "[INFO]: Running Streamlit App"
 export DATABASE_PW="vE0Mj8tx'ND-~fef4%&B"
+export PORT=8501  # Set PORT to 8501 in all cases
 python -m streamlit run app/main.py --server.port $PORT --server.address 0.0.0.0
